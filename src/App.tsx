367b779748a4cdeffa8b77876f8ed8a6c7762c00
Arthur Mauricio <arthur.mdx1234@gmail.com>
2025-03-31 17:31:58 -0300

    seg-3-21

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,26 @@
-import { useState } from 'react';
-import './App.css';
+import { Container } from './components/Container';
+import { Heading } from './components/Heading';
+import './styles/global.css';
+import './styles/theme.css';
 
-function App() {
-  const [count, setCount] = useState(0);
-
+export function App() {
   return (
     <>
-      <div></div>
-      <h1>Vite + React</h1>
-      <div className='card'>
-        <button onClick={() => setCount(count => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className='read-the-docs'>
-        Click on the Vite and React logos to learn more
-      </p>
+      <Container>
+        <Heading>LOGO</Heading>
+      </Container>
+
+      <Container>
+        <Heading>MENU</Heading>
+      </Container>
+
+      <Container>
+        <Heading>FORM</Heading>
+      </Container>
+
+      <Container>
+        <Heading>FOOTER</Heading>
+      </Container>
     </>
   );
 }
-
-export default App;
